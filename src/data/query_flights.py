24720d9c767469c10e9e26ab3adcf6e9e3ea0514
bcbeidel <bcbeidel@gmail.com>
2018-03-05 15:51:16 -0500

    update query for display name

--- src/data/query_flights.py
+++ src/data/query_flights.py
@@ -16,20 +16,22 @@ def flight_and_delay_summary_by_airport(start_date=date.min, end_date=date.max,
     with engine.connect() as conn:
         monthly_flights = pd.read_sql("""
             SELECT 
-                f.OriginAirportID as airport_id,
-                f.Origin as origin_airport,
-                f.OriginCityName as city,
-                f.OriginState as state,
-                COUNT(f.Origin) AS flight_count,
-                SUM(CASE WHEN f.DepDelayMinutes > :delay_threshold THEN 1 ELSE 0 END) AS delayed_count
+                a.display_airport_name as airport_name,
+                f.origin as airport_code,
+                COUNT(f.flights) AS departure_count,
+                SUM(CASE WHEN f.departure_delay > :delay_threshold THEN 1 ELSE 0 END) AS delayed_count
             FROM 
                 flights AS f
+            JOIN
+                airports as a
+            ON
+                a.airport_seq_id = f.origin_airport_sequence_id
             WHERE 
-                f.FlightDate 
+                f.flight_date 
             BETWEEN :start_date 
                 AND :end_date
             GROUP BY 
-                f.Origin
+                f.origin
         """, conn, 
         params={'start_date': start_date, 'end_date': end_date, 'delay_threshold': delay_threshold})
     return monthly_flights
