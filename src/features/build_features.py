3aabe94e13a87ff957966b7f7a416ed1a24b9b18
bcbeidel <bcbeidel@gmail.com>
2018-03-07 22:43:52 -0500

    init build_features

---
+++ src/features/build_features.py
@@ -0,0 +1,14 @@
+""" Return aggregated feature sets for analysis
+"""
+
+
+import sys
+
+
+def main():
+    """Return a fixed dataset, print to console"""
+    pass
+
+
+if __name__ == '__main__':
+        sys.exit(main())
